d6e892a1b66256efab490df16d6b659ed6f65ba1
Manning Unger <manningunger@gmail.com>
2026-02-27 22:45:38 -0500

    File under directory now returns directory if expanded

--- lua/api/file.lua
+++ lua/api/file.lua
@@ -24,11 +24,23 @@ function M.directory_under_cursor(winnr)
   if tbrow_instance_for_window == nil then
     return nil
   end
+  --- @type ViewState
   local view_state = tbrow_instance_for_window.view_state
+  --- @type ModelState
+  local model_state = tbrow_instance_for_window.model_state
   local absolute_path = Actions.file_at_position_default_to_cursor(view_state, winnr)
-  local dir, _ = PathUtil.split_root_and_filename(absolute_path)
-  print(absolute_path .. " split into dir " .. dir)
-  return dir
+  local node = model_state:getFileGraphNodeAtAbsoluteFilepath(absolute_path)
+  if node == nil then
+    error("Invalid state: View pointed at incorrect absolute filepath in position")
+  end
+  -- Allows the user to add to a directory if it has no items in it: if it is expanded,
+  -- return it.
+  if node:isExpanded() then
+    return absolute_path
+  else
+    local dir, _ = PathUtil.split_root_and_filename(absolute_path)
+    return dir
+  end
 end
 
 return M
